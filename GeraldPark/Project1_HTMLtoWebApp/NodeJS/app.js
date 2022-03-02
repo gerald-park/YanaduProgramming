f57d09dadda5fa88639172ea9855cf07322ace25
gerald park <gerald.park@edwardsvacuum.com>
2022-03-03 06:33:48 +0900

    Merge branch 'main' of github.com:gerald-park/YanaduProgramming into main

---
+++ GeraldPark/Project1_HTMLtoWebApp/NodeJS/app.js
@@ -0,0 +1,18 @@
+var a = 1;
+var b = 3;
+var c = a + b;
+console.log(c)
+console.log('Hello World')
+
+var bool = false;
+if(bool){
+    console.log('hi');
+}else
+{
+    console.log('Error')
+}
+
+var arr = [1,5,3,6,3,6,3]
+for (let i of arr){
+    console.log(i)
+}
