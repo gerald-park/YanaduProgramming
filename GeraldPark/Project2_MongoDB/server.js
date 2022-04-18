f42cb96f9cd97b2117f36b9983ecc18823956c0c
gerald park <51350627+cyanluna-git@users.noreply.github.com>
2022-04-18 23:31:11 +0900

    asynchronous programming and synchronuous

--- GeraldPark/Project2_MongoDB/server.js
+++ GeraldPark/Project2_MongoDB/server.js
@@ -1,8 +1,16 @@
+const { json } = require('express');
 const express = require('express');
 const app = express(); 
+app.use(express.json())
 
-app.get('/',function(req,res){
-    return res.send('hello world- nondemon');
+const users = [];
+app.get('/user',function(req,res){
+    return res.send({users:users});
+})
+
+app.post('/user', function(req,res){
+    users.push({name:req.body.name, age:req.body.age})
+    return res.send({success: true})
 })
 
 app.listen(3004,function(){
